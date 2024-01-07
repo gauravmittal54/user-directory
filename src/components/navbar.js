b1c7dc7ebbf95db8e381533043f4ac545d58f751
Gaurav Mittal <61792468+gauravmittal54@users.noreply.github.com>
2024-01-07 14:03:44 +0530

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,71 +1,15 @@
 // components/Navbar.js
 import React, { useState, useEffect, useRef } from 'react';
 import { fetchTimezoneDetails } from '../api';
+import DigitalClock from './clock';
 
 const Navbar = ({ handleGoBack, selectedCountry, handleCountryChange, countries }) => {
-    const [timezoneDetails, setTimezoneDetails] = useState(null);
-    const [clockPaused, setClockPaused] = useState(false);
-    const clockIntervalRef = useRef(null);
-    const lastFetchTimeRef = useRef(null);
-
-    const formatDatetimeString = (datetimeString) => {
-        // Split the datetime string at 'T', take the second part, and then split again at '.'
-        const timeString = datetimeString.split('T')[1].split('.')[0];
-        return timeString;
-    };
+    const [currentCountry, setCurrentCountry] = useState(selectedCountry);
 
     useEffect(() => {
-        const fetchTimezone = async () => {
-            try {
-                if (selectedCountry) {
-                    const details = await fetchTimezoneDetails(selectedCountry);
-                    setTimezoneDetails(details);
-                    lastFetchTimeRef.current = new Date().getTime();
-                }
-            } catch (error) {
-                console.error('Error fetching timezone details:', error);
-            }
-        };
-
-        fetchTimezone();
+        // Update the currentCountry state when selectedCountry changes
+        setCurrentCountry(selectedCountry);
     }, [selectedCountry]);
-
-    useEffect(() => {
-        if (!clockPaused) {
-            const intervalId = setInterval(() => {
-                if (lastFetchTimeRef.current) {
-                    // Calculate time difference since last fetch
-                    const currentTime = new Date().getTime();
-                    const elapsedTime = currentTime - lastFetchTimeRef.current;
-
-                    // Fetch and update the current time
-                    fetchTimezoneDetails(selectedCountry)
-                        .then(details => {
-                            // Update time considering elapsed time
-                            const newTime = new Date(details.datetime).getTime() + elapsedTime;
-                            details.datetime = new Date(newTime).toISOString();
-                            setTimezoneDetails(details);
-
-                            lastFetchTimeRef.current = currentTime;
-                        })
-                        .catch(error => {
-                            console.error('Error updating clock:', error);
-                        });
-                }
-            }, 1000); // Update every second
-
-            clockIntervalRef.current = intervalId;
-
-            return () => {
-                // Cleanup interval when component unmounts
-                clearInterval(clockIntervalRef.current);
-            };
-        }
-    }, [selectedCountry, clockPaused]);
-
-    const handlePauseStartClick = () => {
-        setClockPaused((prevPaused) => !prevPaused);
-    };
 
     return (
         <div id='navbar'>
@@ -82,14 +26,13 @@ const Navbar = ({ handleGoBack, selectedCountry, handleCountryChange, countries 
                     ))}
                 </select>
             </div>
-            {timezoneDetails && (
-        <div id="clock">
-            <span id="clock-text">{formatDatetimeString(timezoneDetails.datetime)}</span>
-        </div>
-    )}
-            <button id='pause-start-button' onClick={handlePauseStartClick}>
-                {'Start/Pause'}
-            </button>
+            {console.log(selectedCountry)}
+            {currentCountry && <div>
+                <span >
+                    <DigitalClock selectedCountry={currentCountry} />
+                </span>
+            </div>}
+            
         </div>
     );
 };
